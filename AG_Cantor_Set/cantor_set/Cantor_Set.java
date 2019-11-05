612a7f9c4b8e85a42f74381a23aa1d9a1ce3762e
JAcciarri <48956574+JAcciarri@users.noreply.github.com>
2019-11-05 12:46:36 -0300

    Update

--- AG_Cantor_Set/cantor_set/Cantor_Set.java
+++ AG_Cantor_Set/cantor_set/Cantor_Set.java
@@ -1,10 +1,13 @@
 package cantor_set;
 
+
 public class Cantor_Set {
-	   
-	private static final int LONGITUD = 243;
-	    private static final int NIVELES = 7;
-	    private static char[][] lines;
+	// LONGITUD LIMITADA A 3 ELEVADO A LA N (3, 9, 27, 81, 243, 729...)   
+	
+	 	static final int LONGITUD = 81;
+	    static final int NIVELES = 5;
+	    static char[][] lines;
+	    static int recursividad = 0;
 	    static {
 	        lines = new char[NIVELES][LONGITUD];
 	        for (int i = 0; i < NIVELES; i++) {
@@ -13,35 +16,46 @@ public class Cantor_Set {
 	            }
 	        }
 	    }	
-	 
-	    public Cantor_Set() {
-	    	cantor(0, LONGITUD, 1);
-	    }
 	   
-	    private static void cantor(int start, int longitud, int index) {
-	        int segmento = longitud / 3;
+	    
+	    public static void main(String[] args) throws InterruptedException {
+	    	
+	    	cantor(0, LONGITUD, 1);
+	    	
+	    	System.out.println("\nGrafica Cantor Set finalizada");
+	    	System.out.println("Nro de recursividades: " + recursividad);
+	 
+	    }
+	    
+	    
+	    static void cantor(int start, int longitud, int index) throws InterruptedException {
+	        recursividad++;
+	    	 int segmento = longitud / 3;
 	        if (segmento == 0) return;
+	       
 	        for (int i = index; i < NIVELES; i++) {
 	            for (int j = start + segmento; j < start + segmento * 2; j++) {
 	                lines[i][j] = ' ';
 	            }
 	        }
+	        
+	        imprimir();
+	      
 	        cantor(start, segmento, index + 1);
 	        cantor(start + segmento * 2, segmento, index + 1);
 	    }
-	 
-	    public static void main(String[] args) throws InterruptedException {
-	    	//funcion recursiva
-	    	cantor(0, LONGITUD, 1);
-	    	//imprimir
-	        for (int i = 0; i < NIVELES; i++) {
+	     
+	    
+	    static void imprimir() throws InterruptedException {
+	    	for (int i = 0; i < NIVELES; i++) {
 	            for (int j = 0; j < LONGITUD; j++) {
 	                System.out.print(lines[i][j]);
 	            }
-	            Thread.sleep(1000);
 	            System.out.println();
-	            System.out.println();
-	        }
+	    	}
+	    	Thread.sleep(300);
+	    	System.out.println();
+	    	System.out.println();
 	    }
-	   
+	    
 }
